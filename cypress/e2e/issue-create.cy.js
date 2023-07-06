404471d031bc9f0955a08f0b0dd8b4b426b81536
ErlendAnderson <erlendanderson@gmail.com>
2023-07-06 19:05:14 +0300

    It was really difficult test, total time spent 4 hours

--- cypress/e2e/issue-create.cy.js
+++ cypress/e2e/issue-create.cy.js
@@ -1,3 +1,8 @@
+import { faker } from '@faker-js/faker';
+
+const randomTitle = faker.word.words();
+const randomSentence = faker.lorem.sentence(20);
+
 describe('Issue create', () => {
   beforeEach(() => {
     cy.visit('/');
@@ -13,8 +18,10 @@ describe('Issue create', () => {
       
       //open issue type dropdown and choose Story
       cy.get('[data-testid="select:type"]').click();
-      cy.get('[data-testid="select-option:Story"]')
-          .trigger('click');
+      //Using wait function which gives website time to load, otherwise proper ticket type can't be selected.
+      cy.wait(1000);
+      cy.get('[data-testid="select-option:Story"]').trigger('click');
+          
             
       //Type value to description input field
       cy.get('.ql-editor').type('TEST_DESCRIPTION');
@@ -48,7 +55,7 @@ describe('Issue create', () => {
           .should('have.length', '5')
           .first()
           .find('p')
-          .contains('TEST_TITLE');
+          .contains('TEST_TITLE')
       //Assert that correct avatar and type icon are visible
       cy.get('[data-testid="avatar:Lord Gaben"]').should('be.visible');
       cy.get('[data-testid="icon:story"]').should('be.visible');
@@ -65,4 +72,115 @@ describe('Issue create', () => {
       cy.get('[data-testid="form-field:title"]').should('contain', 'This field is required');
     });
   });
+
+  //ASSIGNMENT 2: ADD MORE TESTS FOR COVERING ISSUE CREATION FUNCTIONALITY
+  //TEST #1:
+
+  it('Create an issue and validate.', () => {
+    //System finds modal for creating issue and does next steps inside of it
+    cy.get('[data-testid="modal:issue-create"]').within(() => {
+      
+      //open issue type dropdown and choose Bug
+      cy.get('[data-testid="select:type"]').click();
+      //Using wait function which gives website time to load, otherwise proper ticket type can't be selected.
+      cy.wait(1000);
+      cy.get('[data-testid="select-option:Bug"]').trigger('click');
+      
+      //Set bug report priority to Highest
+      cy.get('[data-testid="select:priority"]').click();
+      cy.get('[data-testid="select-option:Highest"]').click();
+      
+      //Type value to description input field
+      cy.get('.ql-editor').type('My bug description');
+
+      //Type value to title input field
+      //Order of filling in the fields is first description, then title on purpose
+      //Otherwise filling title first sometimes doesn't work due to web page implementation
+      cy.get('input[name="title"]').type('BUG');
+      
+      //Select Pickle Rick from reporter dropdown
+      cy.get('[data-testid="select:reporterId"]').click();
+      cy.get('[data-testid="select-option:Pickle Rick"]').click();
+
+      //Click on button "Create issue"
+      cy.get('button[type="submit"]').click();
+    });
+
+    //Assert that modal window is closed and successful message is visible
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
+    
+    //Reload the page to be able to see recently created issue
+    //Assert that successful message has dissappeared after the reload
+    cy.reload();
+    cy.contains('Issue has been successfully created.').should('not.exist');
+
+    //Assert than only one list with name Backlog is visible and do steps inside of it
+    cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
+      //Assert that this list contains 5 issues and first element with tag p has specified text
+      cy.get('[data-testid="list-issue"]')
+          .should('have.length', '5')
+          .first()
+          .find('p')
+          .contains('BUG');
+      //Assert that correct avatar and type icon are visible
+      cy.get('[data-testid="avatar:Pickle Rick"]').should('be.visible');
+      cy.get('[data-testid="icon:bug"]').should('be.visible');
+    });
+  });
+
+  //TEST #2 Add a new test case for creating new issue using random data plugin.
+  //Create a new issue
+
+  it('Create an issue and validate.(Fill fields with Faker)', () => {
+    //System finds modal for creating issue and does next steps inside of it
+    cy.get('[data-testid="modal:issue-create"]').within(() => {
+    
+      //open issue type dropdown and choose Task
+      cy.get('[data-testid="select:type"]').click();
+      cy.contains('[data-testid="select:type"]', 'Task').click();
+      
+      //Set bug report priority to Highest
+      cy.get('[data-testid="select:priority"]').click();
+      cy.get('[data-testid="select-option:Low"]').click();
+      
+      //Type value to description input field
+      cy.get('.ql-editor').type(randomSentence);
+
+      //Type value to title input field
+      //Order of filling in the fields is first description, then title on purpose
+      //Otherwise filling title first sometimes doesn't work due to web page implementation
+      cy.get('input[name="title"]').type(randomTitle);
+      
+      //Select Baby Yoda from reporter dropdown
+      cy.get('[data-testid="select:reporterId"]').click();
+      cy.get('[data-testid="select-option:Baby Yoda"]').click();
+
+      //Click on button "Create issue"
+      cy.get('button[type="submit"]').click();
+    });
+
+    //Assert that modal window is closed and successful message is visible
+    cy.get('[data-testid="modal:issue-create"]').should('not.exist');
+    cy.contains('Issue has been successfully created.').should('be.visible');
+    
+    //Reload the page to be able to see recently created issue
+    //Assert that successful message has dissappeared after the reload
+    cy.reload();
+    cy.contains('Issue has been successfully created.').should('not.exist');
+
+    //Assert than only one list with name Backlog is visible and do steps inside of it
+    cy.get('[data-testid="board-list:backlog').should('be.visible').and('have.length', '1').within(() => {
+      //Assert that this list contains 5 issues and first element with tag p has specified text
+      cy.get('[data-testid="list-issue"]')
+          .should('have.length', '5')
+          .first()
+          .find('p')
+          .contains(randomTitle);
+      //Assert that correct avatar does not exist but type icon is visible
+      cy.get('[data-testid="avatar:Baby Yoda"]').should('not.exist');
+      cy.get('[data-testid="icon:task"]').should('be.visible');
+    });
+  });
+
 });
